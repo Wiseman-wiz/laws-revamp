97741b41c00fa60eb8e24daaa1a9e4699233af02
Jhannuel Ramos <jhannuel.ramos@gmail.com>
2026-02-27 13:38:51 +0800

    refactor: remove unnecessary layout, update imports, and minor UI adjustments

--- src/app/(authenticated)/layout.tsx
+++ src/app/(authenticated)/layout.tsx
@@ -22,10 +22,14 @@ export default async function AuthenticatedLayout({ children }: { children: Reac
     <SidebarProvider defaultOpen={defaultOpen}>
       <AppSidebar user={session.user} caseTypes={caseTypes} />
       <SidebarInset>
-        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12 border-b">
           <div className="flex items-center gap-2 px-4">
             <SidebarTrigger className="-ml-1" />
-            <Separator orientation="vertical" className="mr-2 h-4" />
+            {/* <Separator orientation="vertical" className="mr-2 h-4" /> */}
+            <Separator
+              orientation="vertical"
+              className="mx-2 data-[orientation=vertical]:h-4"
+            />
             <Breadcrumb>
               <BreadcrumbList>
                 <BreadcrumbItem>
@@ -35,7 +39,7 @@ export default async function AuthenticatedLayout({ children }: { children: Reac
             </Breadcrumb>
           </div>
         </header>
-        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+        <div className="flex flex-1 flex-col gap-4 p-4">
           {children}
         </div>
       </SidebarInset>
